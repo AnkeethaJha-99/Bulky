a6b7e45b5506ae541d52571fb05c0e6a18a38adc
AnkeethaJha-99 <ankeethajha#gmail.com>
2023-09-19 11:36:43 +0530

    CRUD Categories

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Bulky.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    option => 
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")
+));
 
 var app = builder.Build();
 
